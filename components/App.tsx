9a454be842332aefd71a11f02fe892d32d7181f1
kliwinska-tech <k.liwinska@gmail.com>
2026-04-29 19:43:25 +0200

    fix paths

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState as useStateReact } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../app/lib/supabase";
 import Auth from "./Auth";
 import { useState } from "react";
 
